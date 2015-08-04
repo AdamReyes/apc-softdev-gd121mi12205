a837f0aeff1f67b705613c9286aeb1b5aed85492
Adam Reyes <masterjonadamreyes@gmail.com>
2015-08-04 12:30:18 +0800

    Started individual_time.cs

---
+++ application/Apothecary/Assets/Codes/individual_timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class individual_timer : MonoBehaviour {
+	public static float individual_time;
+	public static bool individual_time_start;
+
+	// Use this for initialization
+	void Start () {
+		individual_time_start = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Countdown ();
+	}
+
+	// Starts Countdown for customers
+}
