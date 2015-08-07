ea48fb9a9fa0f47372b4805388bd94fa9759f1f4
ChescaEspinola <chescaespinola@gmai.com>
2015-08-08 06:25:19 +0800

    Added touch_two.js for tap control

---
+++ application/Codes v1/touch_two.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+public var speed: float = 0.1F;
+var parti:GameObject;
+static var scoreValue : int = 0;
+var style : GUIStyle;
+
+function Start () {
+
+	scoreValue = 0;
+	
+}
+
+function Update (){
+if (Input.touchCount > 0)
+ 	{
+   var ray = Camera.main.ScreenPointToRay (Input.GetTouch(0).position);
+   var hit : RaycastHit;
+            if (Physics.Raycast (ray, hit)) 
+           {
+            Debug.Log(" You just hit " + hit.collider.gameObject.name);
+            	if(hit.collider.tag == "Part")
+           			 {
+             			Instantiate(parti, hit.point, this.transform.rotation);
+             			Destroy(hit.transform.gameObject);
+             			scoreValue++;
+         
+             			
+            		} 
+           				
+           }
+   	}
+}
+function OnGUI () {
+	
+	GUI.Label(new Rect(750,75,250,100), scoreValue.ToString(), style);
+}
