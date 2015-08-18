0ff3d2b94a25a68511f6df0fd5cdf32937109137
Vince Belleza <vinceyspider@gmail.com>
2015-08-18 12:08:14 +0800

    Added left & right button navigation + placeholder art Added right button code (not yet working)

---
+++ application/Apothecary/Assets/Codes/screen_button_R.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class screen_button_R : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnPointerDown (PointerEventData eventData) {
+
+		transform.Translate (10, 0, 0);
+
+	}
+}
