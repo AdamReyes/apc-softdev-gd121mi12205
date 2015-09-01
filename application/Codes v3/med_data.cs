f1828167a713107b36f4b3e0478c9498c1ac0b3d
Francesca Espinola <chescaespinola@gmail.com>
2015-09-01 21:15:47 +0800

    Added Codes v3 folder for the new script of the game. Added med_data.cs (Data/Symptoms) of the game.

---
+++ application/Codes v3/med_data.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class med_data {
+	public static string[,] dic = new string[,]{
+		{"00101", "Blood spots in the eyes", "Hypertension"}
+		,{"00102", "Facial Flushing", "Hypertension"}
+		,{"00103", "Dizziness", "Hypertension"}
+		,{"00201", "Runny nose", "Cold and Flu"}
+		,{"00202", "Nasal Congestion", "Cold and Flu"}
+		,{"00203", "Fever", "Cold and Flu"}
+		,{"00204", "Body pains", "Cold and Flu"}
+		,{"00301", "Nasal Congestion", "Cough and Cold"}
+		,{"00302", "Itchiness of the throat", "Cough and Cold"}
+		,{"00401", "abdominal pain", "Diarrhea"}
+		,{"00402", "cramping", "Diarrhea"}
+		,{"00403", "bloating", "Diarrhea"}
+		,{"00404", "dehydration", "Diarrhea"}
+		,{"00405", "fever", "Diarrhea"}
+		,{"00406", "bloody stools", "Diarrhea"}
+		,{"00407", "frequent urge to evacuate the bowels", "Diarrhea"}
+		,{"00501", "Itchiness of the skin", "Eczema"}
+		,{"00502", "Rashess on the skin", "Eczema"}
+		,{"00601", "Sweating", "Fever"}
+		,{"00602", "Shivering", "Fever"}
+		,{"00603", "Headache", "Fever"}
+		,{"00604", "Muscle aches", "Fever"}
+		,{"00605", "Loss of appetite", "Fever"}
+		,{"00606", "Dehydration", "Fever"}
+		,{"00607", "General weakness", "Fever"}
+		,{"00608", "Body Temperature 37 C", "Fever"}
+		,{"00701", "Headache", "Nauseau"}
+		,{"00702", "vomiting", "Nauseau"}
+		,{"00703", "dizziness", "Nauseau"}
+		,{"00704", "lightheadedness", "Nauseau"}
+		,{"00705", "diarrhea", "Nauseau"}
+		,{"00706", "abdominal pain", "Nauseau"}
+		,{"00707", "a general feeling of being sick to one's stomach.", "Nauseau"}
+		,{"00801", "Coughing that lasts three or more weeks.", "Tuberculosis"}
+		,{"00802", "Coughing up blood.", "Tuberculosis"}
+		,{"00803", "Chest pain, or pain with breathing or coughing.", "Tuberculosis"}
+		,{"00804", "Unintentional weight loss.", "Tuberculosis"}
+		,{"00805", "Fatigue.", "Tuberculosis"}
+		,{"00806", "Fever.", "Tuberculosis"}
+		,{"00807", "Night sweats.", "Tuberculosis"}
+		,{"00808", "Chills.", "Tuberculosis"}
+
+
+	};
+
+}
